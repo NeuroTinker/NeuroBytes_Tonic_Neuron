d2ff2cbaaad8cdac4f5d7ac2c7049774f0ca0f68
jrwhite <jrwhite20@gmail.com>
2017-08-11 16:15:14 -0500

    add elf

--- FIRMWARE/src/main.c
+++ FIRMWARE/src/main.c
@@ -18,7 +18,7 @@
 #define BUTTON_PRESS_TIME	2
 
 static uint32_t fingerprint[3] __attribute__((section (".fingerprint"))) __attribute__ ((__used__)) = {
-	1, // device id
+	4, // device id
 	1, // firmware version
 	0  // unique id
 };
